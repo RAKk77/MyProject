17b9f4dd681cfe0437e6171568e58917d6a7b842
RAKk77 <yorpheareak168@gmail.com>
2025-01-27 09:25:20 +0700

    Not found Name

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
   return (
     <div className="relative -z-20">
       <div className="w-full">
-        <div className="bg-black rounded-4xl border-2 border-white m-2 p-1">
+        <div className="bg-black rounded-4xl border-2 border-white mt-20 p-2">
           <h1 className="text-col text-white text-[1.6rem] font-bold text-center">
             My Name Is Yorn PheaReak
           </h1>
@@ -13,10 +13,10 @@ const Profile = () => {
         <img
           src={Profiles}
           alt="Profile"
-          className=" border-2 border-white rounded-full my-10"
+          className=" border-2 border-white rounded-full my-4"
         />
       </div>
-      <div className="text-white text-[3rem] flex gap-10 justify-center absolute bottom-[8px] left-[50%] -translate-x-[50%] items-center font-serif">
+      <div className="text-white text-[3rem] flex gap-10 justify-center absolute bottom-[20px] left-[50%] -translate-x-[50%] items-center font-serif">
         <h3 className="anima-one px-[20px] bg-slate-100 text-black font-bold rounded-full absolute right-[5rem] cursor-pointer">
           R
         </h3>
